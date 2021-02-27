38e13f0c5f694b559c858856fbd98c55986aca65
Grga Jovanovski <grga.jovanovski@RA-A029-MBP.local>
2021-02-27 21:02:31 +0100

    CSS fixes

---
+++ collector-code/static/js/chartBattery.js
@@ -0,0 +1,39 @@
+$.ajax({
+    url: "http://vm-iot-school-2020-1.westeurope.cloudapp.azure.com:80/api/telemetry/measurement",
+    data: {
+        "DeviceId": '1',
+        "SensorName": "Battery", 
+        "dateFrom": "2021-02-01",
+        "dateTo": "2022-02-28"
+    },
+    cache: false,
+    type: "GET",
+    success: function (response) {
+        values = [];
+        captions = [];
+        for (var i = 0; i < response.length; i++) {
+            captions[i] = response[i]["CreatedOn"];
+            values[i] = response[i]["SensorValue"];
+        }
+ 
+        var ctx = document.getElementById("Mychart");
+        var mychart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: captions,
+                datasets: [
+                    {
+                        data: values,
+                        label: "Battery",
+                        borderColor: "#3e95cd",
+                        fill: false
+                    },
+                ]
+            }
+        });
+    },
+    error: function (xhr) {
+        console.log("Ajax error!");
+        console.log(xhr);
+    }
+});
